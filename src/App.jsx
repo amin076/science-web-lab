d9e7bd2016d2fcd50e9758206c3bff05411bd549
Amin <amin.nazari076@gmail.com>
2026-07-15 11:03:59 +1000

    fix(platform): remove forbidden evolution custom routes

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,6 @@ import StyleGuide from "@/pages/StyleGuide";
 import SimulationUiShowcase from "@/pages/SimulationUiShowcase";
 import SimulationWorkspaceShowcase from "@/pages/SimulationWorkspaceShowcase";
 import ArtSciencePage from "@/pages/ArtSciencePage";
-import EvolutionOfLifeSimulation from "./simulations/subjects/biology/evolution/evolution-of-life/EvolutionOfLifeSimulation";
 
 // ✅ LayoutShell doesn't need props anymore
 function LayoutShell() {
@@ -145,8 +144,6 @@ export default function App() {
               <Route path="/style-guide" element={<StyleGuide />} />
               <Route path="/simulation-ui-showcase" element={<SimulationUiShowcase />} />
               <Route path="/simulation-workspace-showcase" element={<SimulationWorkspaceShowcase />} />
-          <Route path="/experiments/evolution-of-life" element={<EvolutionOfLifeSimulation />} />
-          <Route path="/experiments/evolution-of-life/run" element={<EvolutionOfLifeSimulation />} />
               <Route path="/art-science" element={<ArtSciencePage />} />
 
               {/* 🔒 Experiments */}
